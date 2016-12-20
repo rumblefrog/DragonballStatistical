f0e5dbd49d674e05dfcf9b6696c972cf5285cde6
RumbleFrog <xxlosttechxx@gmail.com>
2016-12-20 17:13:19 -0500

    Update Log.php

--- Log.php
+++ Log.php
@@ -1,5 +1,5 @@
 <?php
-$version = '1.1.0'; 
+$version = '1.1.0';
 $github_version = ltrim(json_decode(file_get_contents('https://api.github.com/repos/RumbleFrog/DragonballStatistical/releases/latest', false, stream_context_create(array('http'=> array('method'=>"GET", 'header'=>"Accept-language: en\r\n" . "Cookie: foo=bar\r\n" . "User-Agent: DragonballStatistical\r\n")))), true)['tag_name'], 'v');
 $outdated = (version_compare($version, $github_version, ">=")) ? false : true;
 
